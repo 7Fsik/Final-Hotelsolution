396db11fe990932f487485d3167525974253e5b6
inhok-windows11\inhok <kcjkcj0621!>
2023-08-01 01:09:43 +0900

    사진 수정

--- HotelSolution/src/main/java/com/hotelsolution/fire/front/status/controller/StatusController.java
+++ HotelSolution/src/main/java/com/hotelsolution/fire/front/status/controller/StatusController.java
@@ -1,12 +1,23 @@
 package com.hotelsolution.fire.front.status.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.hotelsolution.fire.common.page.vo.PageVo;
 import com.hotelsolution.fire.front.status.service.StatusService;
@@ -24,7 +35,7 @@ public class StatusController {
 	
 	//////////////////////////객실 상태관리 
 	@GetMapping("list")
-	public String roomStatusList(int page, String searchType ,String searchValue, Model model) {
+	public String roomStatusList( @RequestParam(value = "page",defaultValue = "1") int page, String searchType ,String searchValue,String checkSelect, Model model) {
 		
 		int listCount = ss.getRoomCnt();
 		int currentPage = page ;
@@ -33,9 +44,11 @@ public class StatusController {
 		
 		PageVo pv = new PageVo(listCount, currentPage, pageLimit, boardLimit);
 		
-		List<StatusVo>svList = ss.list(pv,searchType,searchValue);
+		List<StatusVo>svList = ss.list(pv,searchType,searchValue,checkSelect);
 		model.addAttribute("pv", pv);
 		model.addAttribute("svList", svList);
+		model.addAttribute("searchType", searchType);
+		model.addAttribute("searchValue", searchValue);
 		
 		return "front/status/list";
 	}
@@ -53,16 +66,46 @@ public class StatusController {
 	
 	
 	//객실 상태 수정
-	@GetMapping("edit")
-	public String roomStatusEdit(StatusVo vo) {
+	@PostMapping("edit")
+	public String roomStatusEdit(String roomIntNo , String introduce , String statusNo ,
+			@RequestParam("img") MultipartFile img , HttpServletRequest req) {
 		
-		int result = ss.roomStatusEdit(vo);
+			StatusVo vo = new StatusVo();
 		
-		if(result != 1) {
-			throw new RuntimeException("상세조회 하다가 에러");
-		}
-		
-		return "redirect : ";
+			if(img != null && !img.isEmpty()) {
+				String savePath = req.getServletContext().getRealPath("/resources/img/front/");
+				MultipartFile file = img ;
+				String eimg = img.getOriginalFilename();
+				String ext = eimg.substring(eimg.lastIndexOf("."));
+				String imgName = UUID.randomUUID().toString()+ext;
+				String path = savePath + imgName;
+				File target = new File(path);
+				try {
+					file.transferTo(target);
+				} catch (IllegalStateException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				vo.setImg(imgName);
+				
+			}
+			vo.setRoomIntNo(roomIntNo);
+			vo.setIntroduce(introduce);
+			vo.setStatusNo(statusNo);
+			
+			
+			
+	        // 나머지 수정 로직 실행
+	        int result = ss.roomStatusEdit(vo);
+
+	        if (result != 1) {
+	            throw new RuntimeException("상세조회 하다가 에러");
+	        }
+
+	        return "redirect:/front/status/list"; 
+	    
 	}
 
 }
--- HotelSolution/src/main/java/com/hotelsolution/fire/front/status/vo/StatusVo.java
+++ HotelSolution/src/main/java/com/hotelsolution/fire/front/status/vo/StatusVo.java
@@ -1,5 +1,7 @@
 package com.hotelsolution.fire.front.status.vo;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Data;
 
 @Data
