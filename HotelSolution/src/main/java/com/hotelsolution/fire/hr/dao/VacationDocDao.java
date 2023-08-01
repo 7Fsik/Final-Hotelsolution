6bfcb83514019c7ca000c047967436880dd6cf1a
DESKTOP-0FE7VLN\user2 <sevenfsik@gmail.com>
2023-08-01 12:50:16 +0900

    vacdoc

---
+++ HotelSolution/src/main/java/com/hotelsolution/fire/hr/dao/VacationDocDao.java
@@ -0,0 +1,25 @@
+package com.hotelsolution.fire.hr.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.hotelsolution.fire.common.page.vo.PageVo;
+import com.hotelsolution.fire.hr.vo.VacationDocVo;
+@Repository
+public class VacationDocDao {
+
+	public List<VacationDocVo> getSubmitList(SqlSessionTemplate sst, Map<String, Object> map) {
+		PageVo pv = (PageVo) map.get("pv");
+		RowBounds rb = new RowBounds(pv.getOffset(), pv.getBoardLimit());
+		return sst.selectList("vacationDoc.getSubmitList",map,rb);
+	}
+
+	public int getSubmitListCnt(SqlSessionTemplate sst, Map<String, Object> map) {
+		return sst.selectOne("vacationDoc.getSubmitListCnt",map);
+	}
+
+}
